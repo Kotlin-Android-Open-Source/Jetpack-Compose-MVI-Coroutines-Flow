9ffef1cd3d5c715091085942f8fd98752f9c03a2
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-05-31 17:34:28 +0700

    init swipe

--- feature-main/src/main/java/com/hoc/flowmvi/ui/main/MainActivity.kt
+++ feature-main/src/main/java/com/hoc/flowmvi/ui/main/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,33 +23,51 @@ import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.DismissDirection.*
+import androidx.compose.material.DismissValue.*
 import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.ListItem
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.ErrorOutline
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -203,6 +225,7 @@ private fun MainContent(
   )
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun UsersList(
   isRefreshing: Boolean,
@@ -210,6 +233,79 @@ private fun UsersList(
   processIntent: IntentDispatcher<ViewIntent>,
   modifier: Modifier = Modifier
 ) {
+
+  LazyColumn {
+    items(userItems) { item ->
+      var unread by  remember{ mutableStateOf(false) }
+      val dismissState = rememberDismissState(
+        confirmStateChange = {
+          if (it == DismissedToEnd) unread = !unread
+          it != DismissedToEnd
+        }
+      )
+      SwipeToDismiss(
+        state = dismissState,
+        modifier = Modifier.padding(vertical = 4.dp),
+        directions = setOf(StartToEnd, EndToStart),
+        dismissThresholds = { direction ->
+          FractionalThreshold(if (direction == StartToEnd) 0.25f else 0.5f)
+        },
+        background = {
+          val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
+          val color by animateColorAsState(
+            when (dismissState.targetValue) {
+              Default -> Color.LightGray
+              DismissedToEnd -> Color.Green
+              DismissedToStart -> Color.Red
+            }
+          )
+          val alignment = when (direction) {
+            StartToEnd -> Alignment.CenterStart
+            EndToStart -> Alignment.CenterEnd
+          }
+          val icon = when (direction) {
+            StartToEnd -> Icons.Default.Done
+            EndToStart -> Icons.Default.Delete
+          }
+          val scale by animateFloatAsState(
+            if (dismissState.targetValue == Default) 0.75f else 1f
+          )
+
+          Box(
+            Modifier
+              .fillMaxSize()
+              .background(color)
+              .padding(horizontal = 20.dp),
+            contentAlignment = alignment
+          ) {
+            Icon(
+              icon,
+              contentDescription = "Localized description",
+              modifier = Modifier.scale(scale)
+            )
+          }
+        },
+        dismissContent = {
+          Card(
+            elevation = animateDpAsState(
+              if (dismissState.dismissDirection != null) 4.dp else 0.dp
+            ).value
+          ) {
+            ListItem(
+              text = {
+                Text(item.toString(), fontWeight = if (unread) FontWeight.Bold else null)
+              },
+              secondaryText = { Text("Swipe me left or right!") }
+            )
+          }
+        }
+      )
+    }
+  }
+
+
+  return
+
   val lastIndex = userItems.lastIndex
 
   SwipeRefresh(
