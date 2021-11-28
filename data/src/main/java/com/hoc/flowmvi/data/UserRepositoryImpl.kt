3d78abe120fd6db6469f96684a96976e3d1249ff
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-11-28 20:09:58 +0700

    <wip:>

--- data/src/main/java/com/hoc/flowmvi/data/UserRepositoryImpl.kt
+++ data/src/main/java/com/hoc/flowmvi/data/UserRepositoryImpl.kt
@@ -36,7 +36,7 @@ import arrow.core.Either.Companion.catch as catchEither
 internal class UserRepositoryImpl @Inject constructor(
   private val userApiService: UserApiService,
   private val dispatchers: CoroutineDispatchers,
-  private val responseToDomain: @JvmSuppressWildcards Mapper<UserResponse, @JvmSuppressWildcards ValidatedNel<UserValidationError, User>>,
+  private val responseToDomain: Mapper<UserResponse, ValidatedNel<@JvmSuppressWildcards UserValidationError, @JvmSuppressWildcards User>>,
   private val domainToBody: Mapper<User, UserBody>,
   private val errorMapper: Mapper<Throwable, UserError>,
 ) : UserRepository {
