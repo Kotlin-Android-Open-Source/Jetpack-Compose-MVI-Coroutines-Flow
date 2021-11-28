0e3740c1fe48b8e5383728f846f2ccbb50161bc6
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-11-28 18:05:41 +0700

    format

--- domain/src/main/java/com/hoc/flowmvi/domain/usecase/GetUsersUseCase.kt
+++ domain/src/main/java/com/hoc/flowmvi/domain/usecase/GetUsersUseCase.kt
@@ -4,8 +4,8 @@ import arrow.core.Either
 import com.hoc.flowmvi.domain.model.User
 import com.hoc.flowmvi.domain.model.UserError
 import com.hoc.flowmvi.domain.repository.UserRepository
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 class GetUsersUseCase @Inject constructor(private val userRepository: UserRepository) {
   operator fun invoke(): Flow<Either<UserError, List<User>>> = userRepository.getUsers()
